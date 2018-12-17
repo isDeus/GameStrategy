7260622d4312b6c5ca89c68e8d8144bbf681607d
Mauricio Campos <m.campos14@ufromail.cl>
2018-12-17 19:58:06 -0300

    Version 1.1

--- src/test/java/com/mycompany/gamestrategy/GameStrategyTest.java
+++ src/test/java/com/mycompany/gamestrategy/GameStrategyTest.java
@@ -32,6 +32,46 @@ public class GameStrategyTest {
         GameStrategy game = new GameStrategy();
         game.ingresarTablero(2, arreglo);
         int[][] resultado = game.mostrarMatriz(game.generarSalida());
-        Assert.assertArrayEquals(resultadoEsperado,resultado);
+        Assert.assertArrayEquals(resultadoEsperado, resultado);
+    }
+
+    @Test
+    public void testGameStrategy2() {
+        String[] arreglo = new String[3];
+        arreglo[0] = "1 b";
+        arreglo[1] = "2 b a";
+        arreglo[2] = "2 ab ac";
+        int[][] resultadoEsperado = new int[3][3];
+        resultadoEsperado[0][0] = 0;
+        resultadoEsperado[0][1] = 1;
+        resultadoEsperado[0][2] = -1;
+        resultadoEsperado[1][0] = 1;
+        resultadoEsperado[1][1] = 0;
+        resultadoEsperado[1][2] = -1;
+        resultadoEsperado[2][0] = 2;
+        resultadoEsperado[2][1] = 2;
+        resultadoEsperado[2][2] = 0;
+        GameStrategy game = new GameStrategy();
+        game.ingresarTablero(3, arreglo);
+        int[][] resultado = game.mostrarMatriz(game.generarSalida());
+        Assert.assertArrayEquals(resultadoEsperado, resultado);
+    }
+
+    @Test
+    public void testGameStategy3() {
+        String[] arreglo = new String[2];
+        arreglo[0] = "2 ab a";
+        arreglo[1] = "1 b";
+
+        int[][] resultadoEsperado = new int[2][2];
+        resultadoEsperado[0][0] = 0;
+        resultadoEsperado[0][1] = -1;
+        resultadoEsperado[1][0] = -1;
+        resultadoEsperado[1][1] = 0;
+
+        GameStrategy game = new GameStrategy();
+        game.ingresarTablero(2, arreglo);
+        int[][] resultado = game.mostrarMatriz(game.generarSalida());
+        Assert.assertArrayEquals(resultadoEsperado, resultado);
     }
 }
